1d533810646cc3d8337af16eb23869399fffd533
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-13 13:55:12 -0400

    Updated from Brython Server: 9/13/2017 1:55:12 PM

--- gradecalculator.py
+++ gradecalculator.py
@@ -1,1 +1,24 @@
+#Astro Ohnuma
+#9/13/17
+#gradecalculator.py - calculating a letter grade from percentages
 
+grade = int(input('Input your grade: '))
+if grade>92:
+    print('You earned an A')
+elif grade>89:
+    print('You earned an A-')
+elif grade>82:
+    print('You earned a B')
+elif grade>79:
+    print('You earned a B-')
+elif grade>72:
+    print('You earned a C')
+elif grade>69:
+    print('You earned a C-')
+elif grade>62:
+    print('You earned a D')
+elif grade>59:
+    print('You earned a D-')
+else:
+    print('You earned an F')
+    
