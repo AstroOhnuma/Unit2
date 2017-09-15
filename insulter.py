8845ac8a65c7f561a2314531972c0ddc2d67570d
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-15 14:54:35 -0400

    Updated from Brython Server: 9/15/2017 2:54:35 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,16 @@
+#Astro Ohnuma
+#9/15/17
+#insulter.py - insulting people randomly!
+
+from random import randint
+name = input('Enter your name: ')
+num1=randint(1,5)
+if num1=1:
+    print('You are a hecking baby')
+elif num1=2:
+    print('Ive met cheesegraters who can do math better than you')
+elif num1=3:
+    print('I know people who eat cereal not for breakfeast who can write better than you')
+elif num1=4:
+    print('If you were a whale, you would have the intelligence of a whale')
 
