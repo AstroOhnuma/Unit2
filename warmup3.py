22fc8e6b9442647e82e7df09c90adbd2aaa8f747
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-15 14:25:10 -0400

    Updated from Brython Server: 9/15/2017 2:25:10 PM

--- warmup3.py
+++ warmup3.py
@@ -1,1 +1,14 @@
+#Astro Ohnuma
+#9/15/17
+#warmup3.py - divisble by 3, 2, neither, or both
+
+num = int(input('Enter a number: '))
+if num%3 == 0 and num%2 == 0:
+    print(num,'is divisible by both')
+elif num%3 == 0:
+    print(num,'is divisble by 3')
+elif num%2 == 0:
+    print(num,'is divisble by 2')
+else:
+    print(num,'is divisble by neither')
 
