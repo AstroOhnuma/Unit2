1fe477fe5363d28d9bff8e467b3314c059d1c7a0
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-18 14:37:02 -0400

    Updated from Brython Server: 9/18/2017 2:37:01 PM

--- insulter.py
+++ insulter.py
@@ -4,13 +4,15 @@
 
 from random import randint
 name = input('Enter your name: ')
-num1=randint(1,5)
-if num1=1:
+num=randint(1,5)
+if num==1:
     print('You are a hecking baby')
-elif num1=2:
+elif num==2:
     print('Ive met cheesegraters who can do math better than you')
-elif num1=3:
+elif num==3:
     print('I know people who eat cereal not for breakfeast who can write better than you')
-elif num1=4:
-    print('If you were a whale, you would have the intelligence of a whale')
+elif num==4:
+    print('If you were a whale, you would have the intelligence of a below average intelligence whale')
+elif num==5:
+    print('If you wera squirrel, you wouldnt know how to store nuts in your cheeks')
 
