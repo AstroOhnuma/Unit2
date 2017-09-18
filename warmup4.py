75e33344575ee68eab0778343a7f1f6d6dce4210
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-18 14:24:15 -0400

    Updated from Brython Server: 9/18/2017 2:24:14 PM

--- warmup4.py
+++ warmup4.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#9/18/17
+#warmup4.py - ask user for a number, print either number or buzz
+
+num = int(input('Enter a number: '))
+
+if '7' in str(num) or num%7 == 0:
+    print('Buzz!')
+else:
+    print(num)
 
