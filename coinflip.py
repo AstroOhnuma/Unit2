a2ee178329c0620a0cb751bbfda9c108c89723a8
AstroOhnuma <31485811+AstroOhnuma@users.noreply.github.com>
2017-09-18 14:42:16 -0400

    Updated from Brython Server: 9/18/2017 2:42:15 PM

--- coinflip.py
+++ coinflip.py
@@ -1,1 +1,11 @@
+#Astro Ohnuma
+#9/18/17
+#coinflip.py - getting either heads or tails when you run the program
+
+from random import randint
+num=randint(1,2)
+if num==1:
+    print('Heads!')
+else:
+    print('Tails!')
 
